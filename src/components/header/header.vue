941658672f433cb9a3fbbc358e61ac19a679e57f
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-10 00:47:48 +0800

    finished the DOM for header component

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -4,6 +4,15 @@
       <div class="avatar">
         <img width="64" height="64" :src="seller.avatar">
       </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+      </div>
     </div>
     <div class="bulletin-wrapper"></div>
   </div>
