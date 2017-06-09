19031c4864d82066ce359076f1c58534648e7b58
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-10 01:58:36 +0800

    added css to title part in header component

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -12,6 +12,10 @@
         <div class="description">
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
+        <div v-if="seller.supports" class="supports">
+          <span class="icon"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
       </div>
     </div>
     <div class="bulletin-wrapper"></div>
@@ -27,5 +31,35 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
+  .header
+    color: #fff
+    background: #000
+    .content-wrapper
+      padding: 24px 12px 18px 24px
+      // to make the empty string between avatar and content go away
+      font-size: 0px
+      .avatar
+        display: inline-block
+      .content
+        display: inline-block
+        margin-left: 16px
+        font-size: 14px
+        .title
+          margin: 2px 0 8px 0
+          .brand
+            display: inline-block
+            vertical-align: top
+            width: 30px
+            height: 18px
+            bg-image('brand')
+            background-size: 30px 18px
+            background-repeat: no-repeat
+          .name
+            margin-left: 6px
+            font-size: 16px
+            line-height: 18px
+            font-weight: bold
 
 </style>
