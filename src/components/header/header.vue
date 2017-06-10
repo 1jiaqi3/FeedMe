a60a8f9dcf7551ec5abe8ed39fda61ad24f66d15
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-11 01:33:41 +0800

    added blurred background image, header component finished

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -26,6 +26,9 @@
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
@@ -44,8 +47,9 @@
   @import "../../common/stylus/mixin.styl"
 
   .header
+    position: relative
     color: #fff
-    background: #999
+    background: rgba(7, 17, 27, 0.5)
     .content-wrapper
       position: relative
       padding: 24px 12px 18px 24px
@@ -145,4 +149,12 @@
         right: 12px
         top: 8px
 
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
 </style>
