833cdef6ad316e7084a95f32a7e73f600322ce6e
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-11 02:14:12 +0800

    added ratingstar component

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -31,7 +31,9 @@
     </div>
     <div class="detail" v-show="detailShow">
       <div class="detail-wrapper clearfix">
-        <div class="detail-main"></div>
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+        </div>
       </div>
       <div class="detail-close">
         <i class="icon-close"></i>
@@ -187,10 +189,16 @@
       background: rgba(7, 17, 27, 0.8)
       // using sticky footer pattern
       .detail-wrapper
+        width: 100%
         min-height: 100%
         .detail-main
           margin-top: 64px
           padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
       .detail-close
         position: relative
         width: 32px
