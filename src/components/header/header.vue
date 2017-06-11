cf4d1096fd45feae4c51622060e09014b4097ea6
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-11 17:36:34 +0800

    added detailed contents in modal

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -42,9 +42,9 @@
             <div class="line"></div>
           </div>
           <ul v-if="seller.supports" class="supports">
-            <li class="support-item" v-for="item in seller.supports">
-              <span class="icon" :class="classMap[seller.supports[$index].type]"></span>
-              <span class="text">{{seller.supports[$index].description}}</span>
+            <li class="support-item" v-for="(item, idx) in seller.supports">
+              <span class="icon" :class="classMap[seller.supports[idx].type]"></span>
+              <span class="text">{{seller.supports[idx].description}}</span>
             </li>
           </ul>
         </div>
@@ -236,6 +236,36 @@
               padding: 0 12px
               font-weight: 700
               font-size: 14px
+          .supports
+            width: 80%
+            margin: 0 auto
+            .support-item
+              padding: 0 12px
+              margin-bottom: 12px
+              font-size: 0
+              &:last-child
+                margin-bottom: 0
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px
+                background-size: 16px 16px
+                background-repeat: no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                line-height: 12px
+                font-size: 12px
       .detail-close
         position: relative
         width: 32px
