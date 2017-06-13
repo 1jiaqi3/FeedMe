89032742e46f236d4ed125bf72dd4fe7b0a13fed
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-13 23:44:03 +0800

    added css style for right menu, specifically on the image and title

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -13,11 +13,11 @@
     <div class="foods-wrapper">
       <ul>
         <li v-for="item in goods" class="food-list">
-          <h1>{{item.name}}</h1>
+          <h1 class="title">{{item.name}}</h1>
           <ul>
-            <li v-for="food in item.foods" class="food-item">
+            <li v-for="food in item.foods" class="food-item border-1px">
               <div class="icon">
-                <img :src="food.icon">
+                <img width="57" height="57" :src="food.icon">
               </div>
               <div class="content">
                 <h2 class="name">{{food.name}}</h2>
@@ -66,7 +66,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  @import "../../common/stylus/mixin"
+  @import "../../common/stylus/mixin.styl"
 
   .goods
     display: flex
@@ -108,7 +108,34 @@
           width: 56px
           vertical-align: middle
           font-size: 12px
-          border-1px(rgba(7, 17, 27, 0.1))
+          border-bottom: 1px solid rgba(7, 17, 27, 0.1)
     .foods-wrapper
       flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147, 153, 159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+        &:last-child
+          border-bottom: 0
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            height: 14px
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7, 17, 27)
 </style>
