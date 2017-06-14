cc0179559ba9029442be3b45ae687088f408222f
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-15 01:52:28 +0800

    added css style for the shopping cart logo

--- src/components/shoppingcart/cart.vue
+++ src/components/shoppingcart/cart.vue
@@ -4,7 +4,7 @@
       <div class="content-left">
         <div class="logo-wrapper">
           <div class="logo">
-            <span class="icon-shopping_cart"></span>
+            <i class="icon-shopping_cart"></i>
           </div>
         </div>
         <div class="price"></div>
@@ -27,5 +27,48 @@
     z-index: 50
     width: 100%
     height: 48px
-    background: #000
+    .content
+      display: flex
+      background: #141d27
+      font-size: 0
+      .content-left
+        flex: 1
+        .logo-wrapper
+          display: inline-block
+          position: relative
+          top: -10px
+          margin: 0 12px
+          padding: 6px
+          width: 56px
+          height: 56px
+          box-sizing: border-box
+          vertical-align: top
+          border-radius: 50%
+          background: #141d27
+          .logo
+            width: 100%
+            height: 100%
+            border-radius: 50%
+            background: #2b343c
+            text-align: center
+            .icon-shopping_cart
+              line-height: 44px
+              font-size: 24px
+              color: #80858a
+        .price
+          display: inline-block
+          vertical-align: top
+          line-height: 24px
+          margin-top: 12px
+          padding-right: 12px
+          box-sizing: border-box
+          border-right: 1px solid rgba(255, 255, 255, 0.1)
+          font-size: 16px
+          font-weight: 700
+        .desc
+          display: inline-block
+      .content-right
+        flex: 0 0 105px
+        width: 105px
+
 </style>
