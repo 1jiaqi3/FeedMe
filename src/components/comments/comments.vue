9093f836ccd81ba8a665a14985b95ab454bfdf01
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-19 01:29:17 +0800

    added and installed comments component to App.vue

--- src/components/comments/comments.vue
+++ src/components/comments/comments.vue
@@ -1,13 +1,64 @@
 <template>
-  <div>
-    I am comment
+  <div class="comments">
+    <div class="comments-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <div class="title">Overall Rating</div>
+          <div class="ranking">Better than {{seller.rankRate}}% restaurants nearby</div>
+        </div>
+        <div class="overview-right">
+          
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  .comments
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
+    .overview
+      display: flex
+      padding: 18px 0
+      .overview-left
+        flex: 0 0 137px
+        padding: 6px 0
+        width: 137px
+        border-right: 1px solid rgba(7, 17, 27, 0.1)
+        text-align: center
+        .score
+          margin-bottom: 6px
+          line-height: 28px
+          font-size: 24px
+          color: rgb(255, 153, 0)
+        .title
+          margin-bottom: 8px
+          line-height: 12px
+          font-size: 12px
+          font-weight: 200
+          color: rgb(7, 17, 27)
+        .ranking
+          line-height: 10px
+          font-size: 10px
+          color: rgb(147, 153, 159)
+      .overview-right
+        flex: 1
+        padding-left: 24px
 
 </style>
