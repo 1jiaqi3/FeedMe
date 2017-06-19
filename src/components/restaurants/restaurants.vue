1700dc4ca05dd3a4520bd1d083dad38c380a67cd
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-20 00:27:00 +0800

    finished up the sylization of upper content of restaurants component

--- src/components/restaurants/restaurants.vue
+++ src/components/restaurants/restaurants.vue
@@ -3,7 +3,7 @@
     <div class="seller-content">
       <div class="overview">
         <h1 class="title">{{seller.name}}</h1>
-        <div class="desc">
+        <div class="desc border-1px">
           <ratingstar :size="36" :score="seller.score"></ratingstar>
           <span class="text">{{seller.ratingCount}}</span>
           <span class="text">{{seller.sellCount}}</span>
@@ -12,19 +12,19 @@
           <li class="block">
             <h2>起送价</h2>
             <div class="content">
-              <span class="stress">${{seller.minPrice}}</span>
+              $<span class="stress">{{seller.minPrice}}</span>
             </div>
           </li>
           <li class="block">
             <h2>Delivery Price</h2>
             <div class="content">
-              <span class="stress">${{seller.deliveryPrice}}</span>
+              $<span class="stress">{{seller.deliveryPrice}}</span>
             </div>
           </li>
           <li class="block">
             <h2>Delivery Time</h2>
             <div class="content">
-              <span class="stress">${{seller.deliveryTime}} min</span>
+              <span class="stress">${{seller.deliveryTime}} </span>min
             </div>
           </li>
         </ul>
@@ -48,5 +48,54 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  @import '../../common/stylus/mixin.styl'
+  .restaurants
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
+    .overview
+      padding: 14px
+      .title
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7, 17, 27)
+        font-size: 14px
+      .desc
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 0
+        .star
+          display: inline-block
+          margin-right: 8px
+          vertical-align: top
+        .text
+          display: inline-block
+          margin-right: 12px
+          line-height: 18px
+          vertical-align: top
+          font-size: 10px
+          color: rgb(77, 85, 93)
+      .remark
+        display: flex
+        padding: 18px
+        .block
+          flex: 1
+          text-align: center
+          border-right: 1px solid rgba(7, 17, 27, 0.1)
+          &:last-child
+            border: none
+          h2
+            margin-bottom: 4px
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .content
+            line-height: 24px
+            font-size: 10px
+            color: rgb(7, 17, 27)
+            .stress
+              font-size: 24px
 </style>
