be93d6b20257749ce5a8440ff6389df3a60d07d9
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-21 01:54:00 +0800

    added heart shaped wishlist toggle to restaurants component

--- src/components/restaurants/restaurants.vue
+++ src/components/restaurants/restaurants.vue
@@ -28,6 +28,10 @@
             </div>
           </li>
         </ul>
+        <div class="fav" @click="toggleFav">
+          <span class="icon-favorite" :class="{'active': favorite}"></span>
+          <span class="text">{{favoriteText}}</span>
+        </div>
       </div>
       <bar></bar>
       <div class="bulletin">
@@ -74,6 +78,16 @@
         type: Object
       }
     },
+    data() {
+      return {
+        favorite: false
+      };
+    },
+    computed: {
+      favoriteText() {
+        return this.favorite ? 'In Wishlist' : 'Add to Wishlist';
+      }
+    },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     },
@@ -118,6 +132,12 @@
             }
           });
         }
+      },
+      toggleFav(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.favorite = !this.favorite;
       }
     },
     components: {
@@ -137,6 +157,7 @@
     width: 100%
     overflow: hidden
     .overview
+      position: relative
       padding: 14px
       .title
         margin-bottom: 8px
@@ -178,6 +199,24 @@
             color: rgb(7, 17, 27)
             .stress
               font-size: 24px
+      .fav
+        position: absolute
+        width: 100px
+        right: 18px
+        top: 18px
+        text-align: center
+        .icon-favorite
+          display: block
+          margin-bottom: 4px
+          line-height: 24px
+          font-size: 24px
+          color: #d4d6d9
+          &.active
+            color: rgb(240, 20, 20)
+        .text
+          line-height: 10px
+          font-size: 10px
+          color: rgb(77, 85, 93)
     .bulletin
       padding: 18px 18px 0 18px
       .title
